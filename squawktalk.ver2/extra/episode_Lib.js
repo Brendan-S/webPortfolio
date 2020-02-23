1e81833ba198be2e9fd1bdf426b448df0f949c2f
Brendan-S <55202537+Brendan-S@users.noreply.github.com>
2020-02-22 23:48:09 -0500

    Create episode_Lib.js

---
+++ squawktalk.ver2/extra/episode_Lib.js
@@ -0,0 +1,148 @@
+
+
+var returnContainer = document.querySelector("return1"); 
+
+var returnContainerId = document.querySelector("#returnID"); 
+
+var episode1Container = document.querySelector("#episode1ID");
+
+var episode2Container = document.querySelector("#episode2ID");
+
+var episode3Container = document.querySelector(".episode3Class");
+
+var episode4Container = document.querySelector("#episode4ID");
+
+var episode5Container = document.querySelector("#episode5ID");
+
+var episode6Container = document.querySelector("#episode6ID");
+
+var episode7Container = document.querySelector("#episode7ID");
+
+var episode8Container = document.querySelector("#episode8ID");
+
+
+function returnColor(){
+
+    returnContainerId.style.color = "#E3EA96"; 
+    returnContainerId.style.transition = "color ease-in-out 1s";
+
+    returnContainer.style.color = "#E3EA96"; 
+    returnContainer.style.transition = "color ease-in-out 1s";
+
+}; 
+
+function removeColor(){
+
+    returnContainerId.style.color = "white"; 
+    returnContainerId.style.transition = "color ease-in-out 1s";
+
+    returnContainer.style.color = "white"; 
+    returnContainer.style.transition = "color ease-in-out 1s";
+}; 
+
+function colorChange1(){
+
+episode1Container.style.color = "#E3EA96"; 
+episode1Container.style.transition = "color ease-in-out 1s";
+
+};
+
+
+function colorRemove1(){
+episode1Container.style.color = "white"; 
+episode1Container.style.transition = "color ease-in-out 1s";
+}
+
+
+function colorChange2(){
+
+episode2Container.style.color = "#E3EA96";
+episode2Container.style.transition = "color ease-in-out 1s";
+
+};
+
+function colorRemove2(){
+    episode2Container.style.color = "white"; 
+    episode2Container.style.transition = "color ease-in-out 1s";
+}
+    
+
+
+function colorChange3(){
+
+episode3Container.style.color = "#E3EA96"; 
+episode3Container.style.transition = "color ease-in-out 1s";
+
+};
+
+function colorRemove3(){
+    episode3Container.style.color = "white"; 
+    episode3Container.style.transition = "color ease-in-out 1s";
+}
+    
+
+function colorChange4(){
+
+episode4Container.style.color = "#E3EA96";
+episode4Container.style.transition = "color ease-in-out 1s";
+
+};
+
+function colorRemove4(){
+    episode4Container.style.color = "white"; 
+    episode4Container.style.transition = "color ease-in-out 1s";
+}
+    
+
+function colorChange5(){
+
+episode5Container.style.color = "#E3EA96";
+episodeContainer5.style.transition = "color ease-in-out 1s";
+};
+
+function colorRemove5(){
+    episode5Container.style.color = "white"; 
+    episode5Container.style.transition = "color ease-in-out 1s";
+   
+}
+    
+
+
+function colorChange6(){
+
+episode6Container.style.color = "#E3EA96";
+episode6Container.style.transition = "color ease-in-out 1s";
+};
+
+function colorRemove6(){
+    episode6Container.style.color = "white"; 
+    episode6Container.style.transition = "color ease-in-out 1s";
+}
+    
+
+
+function colorChange7(){
+
+episode7Container.style.color = "#E3EA96"; 
+episode7Container.style.transition = "color ease-in-out 1s";
+}; 
+
+function colorRemove7(){
+    episode7Container.style.color = "white"; 
+    episode7Container.style.transition = "color ease-in-out 1s";
+}
+    
+
+function colorChange8(){
+
+episode8Container.style.color = "#E3EA96";
+episode8Container8.style.transition = "color ease-in-out 1s";
+
+};
+
+function colorRemove8(){
+    episode8Container.style.color = "white"; 
+    episode8Container.style.transition = "color ease-in-out 1s";
+}
+    
+
