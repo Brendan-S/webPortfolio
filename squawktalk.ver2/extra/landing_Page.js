9699f909803ae7f0470cd385d561b3062f88629c
Brendan-S <55202537+Brendan-S@users.noreply.github.com>
2020-02-22 23:47:59 -0500

    Create landing_Page.js

---
+++ squawktalk.ver2/extra/landing_Page.js
@@ -0,0 +1,18 @@
+
+
+var colorChange = document.querySelector("#archive"); 
+//"#F0A136"
+
+function wordColor(){
+
+colorChange.style.color =  "#EBC829"; 
+colorChange.style.transition = "color ease-in-out 1s";
+
+}; 
+
+function colorRemove(){
+    
+colorChange.style.color = "white"; 
+colorChange.style.transition = "color ease-in-out 1s";
+
+}; 
